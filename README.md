891982514b2babee4b8df030112960e8ad0353f2
Flominator <github.flominatorgd@gmail.com>
2026-03-14 16:15:06 +0100

    Add extras for j5y17ltexx-user 7.0 NRD90M J530FXXU2ARG2 release-keys
    Signed-off-by: Flominator <github.flominatorgd@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## j5y17ltexx-user 7.0 NRD90M J530FXXU2ARG2 release-keys
+- Manufacturer: samsung
+- Platform: exynos5
+- Codename: j5y17lte
+- Brand: samsung
+- Flavor: j5y17ltexx-user
+- Release Version: 7.0
+- Id: NRD90M
+- Incremental: J530FXXU2ARG2
+- Tags: release-keys
+- CPU Abilist: armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 320
+- Fingerprint: samsung/j5y17ltexx/j5y17lte:7.0/NRD90M/J530FXXU2ARG2:user/release-keys
+- OTA version: 
+- Branch: j5y17ltexx-user-7.0-NRD90M-J530FXXU2ARG2-release-keys
+- Repo: samsung_j5y17lte_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat system/app/SBrowser_6.2/SBrowser_6.2.apk.* 2>/dev/null >> system/app/SBrowser_6.2/SBrowser_6.2.apk
+rm -f system/app/SBrowser_6.2/SBrowser_6.2.apk.* 2>/dev/null
+cat system/app/Chrome/Chrome.apk.* 2>/dev/null >> system/app/Chrome/Chrome.apk
+rm -f system/app/Chrome/Chrome.apk.* 2>/dev/null
+cat system/priv-app/OneDrive_Samsung_v2/OneDrive_Samsung_v2.apk.* 2>/dev/null >> system/priv-app/OneDrive_Samsung_v2/OneDrive_Samsung_v2.apk
+rm -f system/priv-app/OneDrive_Samsung_v2/OneDrive_Samsung_v2.apk.* 2>/dev/null
+cat system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/priv-app/Velvet/Velvet.apk
+rm -f system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/priv-app/SHealth5/SHealth5.apk.* 2>/dev/null >> system/priv-app/SHealth5/SHealth5.apk
+rm -f system/priv-app/SHealth5/SHealth5.apk.* 2>/dev/null
+cat system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null >> system/priv-app/StickerFaceAR/StickerFaceAR.apk
+rm -f system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null
